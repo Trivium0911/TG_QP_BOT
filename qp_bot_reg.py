2e5f7ecdf51731426b371f727126752f8e4350fc
Trivium0911 <triv_1993@mail.ru>
2022-08-05 02:49:14 +0300

    fixed 5

--- qp_bot_reg.py
+++ qp_bot_reg.py
@@ -80,7 +80,8 @@ def main():
 
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     main()
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 
 
