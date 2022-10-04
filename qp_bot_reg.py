20450b3574d1708f1483c86cd7e0568453a88ce3
Trivium0911 <triv_1993@mail.ru>
2022-10-04 17:20:03 +0300

    included ngrok tunnel link

--- qp_bot_reg.py
+++ qp_bot_reg.py
@@ -1,3 +1,6 @@
+from aiogram.dispatcher.webhook import get_new_configured_app
+from aiohttp import web
+
 from main import get_actual_games, get_ids, post_inf
 from aiogram.utils.executor import start_webhook
 from aiogram.dispatcher.filters import Text
@@ -16,12 +19,12 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.onrender.com'
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBHOOK_PATH = f''
+WEBHOOK_URL = f'https://8aaa-46-56-247-37.ngrok.io'
 
 # webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_HOST = '127.0.0.1'
+WEBAPP_PORT = os.getenv('PORT', default=5000)
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -35,7 +38,12 @@ async def on_startup(dispatcher):
 
 
 async def on_shutdown(dispatcher):
+    logging.warning('Shutting down..')
     await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
 
 
 @dp.message_handler(commands='start')
@@ -93,16 +101,17 @@ async def make_form(message: types.Message):
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        skip_updates=False,
+        skip_updates=True,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
+    # app = get_new_configured_app(dispatcher=dp, path=f'/webhook/{BOT_TOKEN}')
+    # web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
 
 
 if __name__ == '__main__':
