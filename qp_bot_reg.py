dacfd66f7c7c7a2077936825761da7f7dfe9f9bb
Trivium0911 <triv_1993@mail.ru>
2022-10-04 17:54:27 +0300

    update

--- qp_bot_reg.py
+++ qp_bot_reg.py
@@ -19,8 +19,8 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.onrender.com'
-WEBHOOK_PATH = f''
-WEBHOOK_URL = f'https://8aaa-46-56-247-37.ngrok.io'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
