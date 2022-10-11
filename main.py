c3cfb90b2f63a99eb17f9be0627021fc76c64f88
Trivium0911 <triv_1993@mail.ru>
2022-10-11 09:13:46 +0300

    update

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
-headers = {
-    "accept": '*/*',
-    # "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-    'x-client-data': 'CIq2yQEIo7bJAQjBtskBCKmdygEIlKHLAQiLq8wBCP+7zAEI2LzMAQiywcwBCMTBzAEI18HMAQ=='
-}
 
 URL = os.getenv('URL')
 URL_POST = os.getenv('URL_POST')
